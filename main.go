0df1cdb3c9ab7be926c07465ba77c6b4559cb0a2
Nick Miller <nickmiller.on@gmail.com>
2018-10-03 23:28:23 +0300

    get institutes and groups

--- main.go
+++ main.go
@@ -15,6 +15,67 @@ func HomeRouterHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!")
 }
 
+func InstitutesHandler(w http.ResponseWriter, r *http.Request) {
+	var institutes []string
+	err := col.Find(nil).Distinct("institute", &institutes)
+	if err != nil {
+		fmt.Fprintf(w, "Internal server error")
+		return
+	}
+	jInst, err := json.Marshal(institutes)
+	if err == nil {
+		fmt.Fprintf(w, string(jInst))
+	}
+}
+
+func RootGroupsHandler(w http.ResponseWriter, r *http.Request) {
+	var rootGroups []string
+
+	inst, ok := r.URL.Query()["inst"]
+	if ok && len(inst[0]) > 0 {
+		err := col.Find(bson.M{"institute":inst[0]}).Distinct("rootgroup", &rootGroups)
+		if err != nil {
+			fmt.Fprintf(w, "Groups not found")
+			return
+		}
+	} else {
+		err := col.Find(nil).Distinct("rootgroup", &rootGroups)
+		if err != nil {
+			fmt.Fprintf(w, "Groups not found")
+			return
+		}
+	}
+
+	jRootGroups, err := json.Marshal(rootGroups)
+	if err == nil {
+		fmt.Fprintf(w, string(jRootGroups))
+	}
+}
+
+func GroupsHandler(w http.ResponseWriter, r *http.Request) {
+	var groups []string
+	rGroup, ok := r.URL.Query()["rootGroup"]
+
+	if ok && len(rGroup[0]) > 0 {
+		err := col.Find(bson.M{"rootgroup":rGroup[0]}).Distinct("name", &groups)
+		if err != nil {
+			fmt.Fprintf(w, "Groups not found")
+			return
+		}
+	} else {
+		err := col.Find(nil).Distinct("name", &groups)
+		if err != nil {
+			fmt.Fprintf(w, "Groups not found")
+			return
+		}
+	}
+
+	jGroups, err := json.Marshal(groups)
+	if err == nil {
+		fmt.Fprintf(w, string(jGroups))
+	}
+}
+
 func SclUpdateHandler(w http.ResponseWriter, r *http.Request) {
 	year, ok := r.URL.Query()["year"]
 	if !ok || len(year[0]) < 1 {
@@ -29,7 +90,6 @@ func SclUpdateHandler(w http.ResponseWriter, r *http.Request) {
 		scl.UpdateFormSite(inst[0], year[0])
 	}
 }
-
 func SclHandler(w http.ResponseWriter, r *http.Request) {
 	year, ok := r.URL.Query()["year"]
 	if !ok || len(year[0]) < 1 {
@@ -64,9 +124,7 @@ func SclHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 }
-
 var col *mgo.Collection
-
 func main() {
 	session, err := mgo.Dial("localhost")
 	if err != nil {
@@ -82,6 +140,9 @@ func main() {
 	http.HandleFunc("/", HomeRouterHandler)
 	http.HandleFunc("/scl", SclHandler)
 	http.HandleFunc("/scl/update", SclUpdateHandler)
+	http.HandleFunc("/institutes", InstitutesHandler)
+	http.HandleFunc("/groups", GroupsHandler)
+	http.HandleFunc("/rootGroups", RootGroupsHandler)
 
 	err = http.ListenAndServe(":9000", nil)
 	if err != nil {
